bfcf86ceec324cdd95c4a7edd7553a15c6900b9c
nazem0_home_pc <mohamednazem09@gmail.com>
2023-09-27 23:12:10 +0300

    added 404 page

--- src/app/Modules/app.module.ts
+++ src/app/Modules/app.module.ts
@@ -8,6 +8,7 @@ import { LoginComponent } from './components/login/login.component';
 import { VendorsPageComponent } from './components/vendors-page/vendors-page.component';
 import { VendorProfilePageComponent } from './components/vendor-profile-page/vendor-profile-page.component';
 import { CreateOfferComponent } from './components/create-offer/create-offer.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
@@ -17,6 +18,7 @@ import { CreateOfferComponent } from './components/create-offer/create-offer.com
     VendorsPageComponent,
     VendorProfilePageComponent,
     CreateOfferComponent,
+    PageNotFoundComponent,
   ],
   imports: [
     BrowserModule,
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,13 +5,15 @@ import { LoginComponent } from './Modules/components/login/login.component';
 import { VendorsPageComponent } from './Modules/components/vendors-page/vendors-page.component';
 import { VendorProfilePageComponent } from './Modules/components/vendor-profile-page/vendor-profile-page.component';
 import { CreateOfferComponent } from './Modules/components/create-offer/create-offer.component';
+import { PageNotFoundComponent } from './Modules/components/page-not-found/page-not-found.component';
 
 const routes: Routes = [
-  {path:'register', component:RegisterComponent},
-  {path:'login', component:LoginComponent},
-  {path:'vendors', component:VendorsPageComponent},
-  {path:'vendor-profile', component:VendorProfilePageComponent},
-  {path:'create-offer',component:CreateOfferComponent}
+  { path: 'register', component: RegisterComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'vendors', component: VendorsPageComponent },
+  { path: 'vendor-profile', component: VendorProfilePageComponent },
+  { path: 'create-offer', component: CreateOfferComponent },
+  { path: '**', component: PageNotFoundComponent }
 ];
 
 @NgModule({
