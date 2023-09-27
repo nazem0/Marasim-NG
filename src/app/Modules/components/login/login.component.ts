55534bec85447c01c8fd7f6d21affb2e1f9a6719
nazem0_home_pc <mohamednazem09@gmail.com>
2023-09-27 23:12:04 +0300

    adjusted animation

--- src/app/Modules/components/login/login.component.ts
+++ src/app/Modules/components/login/login.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { ScrollRevealService } from './../../../services/scroll-reveal.service';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  constructor(private ScrollReveal: ScrollRevealService ){
 
+  }
+ngOnInit(){
+  const sr = this.ScrollReveal.getScrollReveal();
+  sr.reveal('.rightSide', { origin: 'right', duration: 1200,distance: '500px' })
+  sr.reveal('.leftSide', { origin: 'left', duration: 1200 ,distance: '500px'})
+}
 }
--- src/app/Modules/components/register/register.component.ts
+++ src/app/Modules/components/register/register.component.ts
@@ -10,7 +10,6 @@ export class RegisterComponent implements OnInit {
   constructor(private ScrollReveal: ScrollRevealService) { }
   ngOnInit() {
     const sr = this.ScrollReveal.getScrollReveal();
-    let inputRevelDuration=1500;
     sr.reveal('.rightSide', { origin: 'right', duration: 1200,distance: '500px' })
     sr.reveal('.leftSide', { origin: 'left', duration: 1200 ,distance: '500px'})
 
