19abcc4f4ee579cab319002f740b28141a061438
Samer Said <supsamer2009@yahoo.com>
2023-10-09 19:21:43 +0300

    rename c c c c c c c

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,10 @@ import { LoginComponent } from './Shared/components/login/login.component';
 import { PageNotFoundComponent } from './Shared/components/page-not-found/page-not-found.component';
 import { ContactComponent } from './Shared/components/contact/contact.component';
 import { HomeComponent } from './Shared/components/home/home.component';
-import { PendingComponent } from './Vendor/Components/pending/pending.component';
+import { PendingComponent } from './Vendor/components/pending/pending.component';
 import { NavBarComponent } from './Shared/components/nav-bar/nav-bar.component';
-import { EditServiceComponent } from './Vendor/Components/edit-service/edit-service.component';
-import { AddServiceComponent } from './Vendor/Components/add-service/add-service.component';
+import { EditServiceComponent } from './Vendor/components/edit-service/edit-service.component';
+import { AddServiceComponent } from './Vendor/components/add-service/add-service.component';
 
 
 @NgModule({
