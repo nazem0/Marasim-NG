264e243f40252382c09fb103c60f3cc52c2709ef
Samer Said <supsamer2009@yahoo.com>
2023-10-09 17:59:15 +0300

    home compoment routhing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,8 +3,10 @@ import { RouterModule, Routes } from '@angular/router';
 import { RegisterComponent } from './Shared/components/register/register.component';
 import { LoginComponent } from './Shared/components/login/login.component';
 import { PageNotFoundComponent } from './Shared/components/page-not-found/page-not-found.component';
+import { HomeComponent } from './Shared/components/home/home.component';
 
 const routes: Routes = [
+  { path: '', component: HomeComponent},
   { path: 'register', component: RegisterComponent },
   { path: 'login', component: LoginComponent },
   {
