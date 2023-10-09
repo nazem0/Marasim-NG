ff07ade78b8d7b84976e227a560952b9e9c9ccb6
Samer Said <supsamer2009@yahoo.com>
2023-10-09 17:58:46 +0300

    vendor services make over and home routhing

--- src/app/Vendor/Components/vendor-services/vendor-services.component.ts
+++ src/app/Vendor/Components/vendor-services/vendor-services.component.ts
@@ -6,5 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./vendor-services.component.css']
 })
 export class VendorServicesComponent {
+  User = {
+    Name: 'أحمد محمد خالد',
+    Category: 'مصور فوتوغرافى'
+  }
+
+  CARDS: any[] = [
+    { title: 'تصوير افراح', srcImg: '../assets/img/hall.webp' },
+    { title: 'تصوير ', srcImg: '../assets/img/photo_session.webp' },
+    { title: 'تصوير افراح', srcImg: '../assets/img/chefs.webp' },
+    { title: 'تصوير ', srcImg: '../assets/img/hairdresser.webp' },
+    { title: 'تصوير افراح', srcImg: '../assets/img/club.webp' }
+  ];
 
 }
