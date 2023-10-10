640418099557f9cb76a0727c4069adeb7564453d
nazem0 <mohamednazem09@gmail.com>
2023-10-10 16:54:59 +0300

    gend packs

---
+++ src/app/User/components/generated-packages/generated-package-card/generated-package-card.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-generated-package-card',
+  templateUrl: './generated-package-card.component.html',
+  styleUrls: ['./generated-package-card.component.css']
+})
+export class GeneratedPackageCardComponent {
+
+}
