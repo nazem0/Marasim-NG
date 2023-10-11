3ff6bfc1a64585c2a0d64ad1698e550d2fb45764
nazem0_home_pc <mohamednazem09@gmail.com>
2023-10-11 21:57:52 +0300

    post interface

--- src/app/Models/IPost.ts
+++ src/app/Models/IPost.ts
@@ -1,28 +1,28 @@
 export interface IPost {
-    id: number;
-    title: string; 
-    description: string;
-    date: string;
+    id: number,
+    title: string, 
+    description: string,
+    date: string,
   }
   
   export interface IReaction {
-    id: number;
-    userId: number;
-    postId: number;
-    dateTime: string;
-    value?: string; 
+    id: number,
+    userId: number,
+    postId: number,
+    dateTime: string,
+    value?: string, 
   }
   
   export interface IComment {
-    id: number;
-    userId: number;  
-    postId: number;
-    text: string;
-    dateTime: string;
+    id: number,
+    userId: number,  
+    postId: number,
+    text: string,
+    dateTime: string,
   }
   
   export interface IAttachment {
-    id: number;
-    postId: number;
-    attachmentUrl: string;
+    id: number,
+    postId: number,
+    attachmentUrl: string,
   }
