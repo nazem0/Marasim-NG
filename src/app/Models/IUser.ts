d662039ea776159010e26be672e07b33b2ad63da
nazem0_home_pc <mohamednazem09@gmail.com>
2023-10-11 21:55:35 +0300

    IUser

--- src/app/Models/IUser.ts
+++ src/app/Models/IUser.ts
@@ -1,13 +1,13 @@
 export interface IUser {
-    "id": number, 
-    "name": string,
-    "phone": string,
-    "email": string,
-    "password": string,
-    "photo": string,
-    "gender": string,
-    "role": string,
-    "nationalID": string
+    id: number, 
+    name: string,
+    phone: string,
+    email: string,
+    password: string,
+    photo: string,
+    gender: string,
+    role: string,
+    nationalID: string
 }
 
 
