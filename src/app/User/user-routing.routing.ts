a11ff8686e385edf37d27144edfde70ae2ea7b2f
Samer Said <supsamer2009@yahoo.com>
2023-10-11 21:37:57 +0300

    vendor profile route

--- src/app/User/user-routing.routing.ts
+++ src/app/User/user-routing.routing.ts
@@ -12,22 +12,24 @@ import { ContactComponent } from '../Shared/components/contact/contact.component
 import { LoginComponent } from '../Shared/components/login/login.component';
 import { RegisterComponent } from '../Shared/components/register/register.component';
 import { FeedComponent } from './components/feed/feed.component';
+import { VendorProfileComponent } from './components/vendor-profile/vendor-profile.component';
 
 const routes: Routes = [
   {
-    path:'',component:LayoutComponent,children:[
+    path: '', component: LayoutComponent, children: [
+      { path: '', component: HomeComponent },
       { path: 'check-list', component: UserCheckListComponent },
       { path: 'invitation-list', component: InvitationListComponent },
       { path: 'edit', component: UserEditProfileComponent },
       // { path: 'profile-edit', component: UserProfileEditComponent }, //to vendor
       { path: 'invitation', component: InvitationComponent },
-      {path:'generate-packages',component:GeneratePackagesComponent},
-      {path:'generated-packages',component:GeneratedPackagesComponent},
-      {path:'',component:HomeComponent},
+      { path: 'generate-packages', component: GeneratePackagesComponent },
+      { path: 'generated-packages', component: GeneratedPackagesComponent },
       { path: 'register', component: RegisterComponent },
       { path: 'login', component: LoginComponent },
       { path: 'contact', component: ContactComponent },
-      { path:'feed',component:FeedComponent}
+      { path: 'feed', component: FeedComponent },
+      { path: 'vendor-profile', component: VendorProfileComponent }
     ]
   }
 
