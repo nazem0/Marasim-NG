2e43f61f223bdc9979f447bde4c375f3ff464802
nazem0_home_pc <mohamednazem09@gmail.com>
2023-10-12 20:13:53 +0300

    please don't use any :C

--- src/app/Services/Vendor.service.ts
+++ src/app/Services/Vendor.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { IVendor } from '../Models/IVendor';
 import { Observable, forkJoin } from 'rxjs';
+import { IUser } from '../Models/IUser';
 
 @Injectable({
     providedIn: 'root'
@@ -10,9 +11,9 @@ export class VendorService {
     constructor(private http: HttpClient) {
     }
     
-    public getVendorWithUser(vendorId: number, userId: number): Observable<any[]> {
-        let response1 = this.http.get(`http://localhost:3000/Vendor/${vendorId}`);
-        let response2 = this.http.get(`http://localhost:3000/User/${userId}`);
+    public getVendorWithUser(vendorId: number, userId: number): Observable<[IVendor,IUser]> {
+        let response1 = this.http.get<IVendor>(`http://localhost:3000/Vendor/${vendorId}`);
+        let response2 = this.http.get<IUser>(`http://localhost:3000/User/${userId}`);
         console.log("response 1 :  " + response1 + "\n response 2 :  " +response2)
         return forkJoin([response1, response2]);
       }
