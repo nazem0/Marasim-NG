282c01b85afc726db225505922f9261a2f2eac40
nazem0_home_pc <mohamednazem09@gmail.com>
2023-10-13 00:51:34 +0300

    vendor card

---
+++ src/app/Shared/components/vendor-card/vendor-card.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-vendor-card',
+  templateUrl: './vendor-card.component.html',
+  styleUrls: ['./vendor-card.component.css']
+})
+export class VendorCardComponent {
+
+}
