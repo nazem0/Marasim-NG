f5d77556825af5ec13057563c200e04aaf51f683
nazem0_home_pc <mohamednazem09@gmail.com>
2023-10-13 23:22:36 +0300

    expanding, shrinking filter

--- src/app/User/components/feed/feed.component.ts
+++ src/app/User/components/feed/feed.component.ts
@@ -1,25 +1,39 @@
-import { Component } from '@angular/core';
 import { IPost } from 'src/app/Models/IPost';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { PostService } from 'src/app/Services/Post.service';
+
 
 @Component({
   selector: 'app-feed',
   templateUrl: './feed.component.html',
   styleUrls: ['./feed.component.css']
 })
-export class FeedComponent {
+export class FeedComponent implements OnInit,AfterViewInit {
   posts: IPost[] | null = null;
-  constructor(private post: PostService) {
+  @ViewChild("filterContainer") filterContainer!: ElementRef;
+  @ViewChild("caret") caret!: ElementRef;
 
+  constructor(private postService: PostService) { }
+  ngAfterViewInit(): void {
+    console.log(this.filterContainer.nativeElement);
+  }
 
+  ngOnInit(): void {
+    this.postService.get().subscribe((result: IPost[]) => {
+      this.posts = result;
+      console.log(this.posts);
+    });
+  }
 
-    this.post.get().subscribe({
-      next: (result: IPost[]) => {
-        this.posts = result;
-        console.log(this.posts);
-      }
-    })
-
+  showFilter() {
+    if (this.filterContainer.nativeElement.classList.contains('expanded')) {
+      this.caret.nativeElement.classList.add('fa-caret-left')
+      this.caret.nativeElement.classList.remove('fa-caret-right')
+      this.filterContainer.nativeElement.classList.remove('expanded');
+    } else {
+      this.caret.nativeElement.classList.remove('fa-caret-left')
+      this.caret.nativeElement.classList.add('fa-caret-right')
+      this.filterContainer.nativeElement.classList.add('expanded');
+    }
   }
 }
-
