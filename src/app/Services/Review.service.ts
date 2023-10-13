277004dce18245b0bbaf52813b7a213057ee31df
Samer Said <supsamer2009@icloud.com>
2023-10-13 17:25:25 +0300

    review edit

--- src/app/Services/Review.service.ts
+++ src/app/Services/Review.service.ts
@@ -1,20 +1,22 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, forkJoin } from 'rxjs';
 import { IReview } from '../Models/IReview';
+import { IVendor } from '../Models/IVendor';
+import { IUser } from '../Models/IUser';
 
 @Injectable({ providedIn: 'root' })
 export class ReviewService {
-  constructor(private httpClient: HttpClient) { }
+  constructor(private http: HttpClient) { }
 
   get():Observable<IReview[]> {
-    return this.httpClient.get<IReview[]>("http://localhost:3000/reviews")
+    return this.http.get<IReview[]>("http://localhost:3000/reviews")
   }
 
   getByUserID(ID:number):Observable<IReview[]>
   {
     if(ID){
-      return this.httpClient.get<IReview[]>(`http://localhost:3000/reviews/?userId=${ID}`)
+      return this.http.get<IReview[]>(`http://localhost:3000/reviews/?userId=${ID}`)
     }
     else{
       throw "No Reviews";
