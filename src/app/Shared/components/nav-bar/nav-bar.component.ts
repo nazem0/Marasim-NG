c0adb0a1dda516c774f0e6867df9c398d396955e
Sohayb <engsohayb97@gmail.com>
2023-10-15 15:38:11 +0300

    nav-bar

--- src/app/Shared/components/nav-bar/nav-bar.component.ts
+++ src/app/Shared/components/nav-bar/nav-bar.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { AuthService } from 'src/app/Services/Auth.service';
 
 @Component({
   selector: 'app-nav-bar',
   templateUrl: './nav-bar.component.html',
   styleUrls: ['./nav-bar.component.css']
 })
-export class NavBarComponent {
-
+export class NavBarComponent   {
+  isLogged:boolean=false;
+  constructor(private AuthService:AuthService){}
+  ngOnInit(){
+    this.isLogged=this.AuthService.loggedIn;
+  }
 }
