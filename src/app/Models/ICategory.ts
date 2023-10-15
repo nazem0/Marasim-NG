8be39fcac0ec7a997026b263c399362976313d5d
Samer Said <supsamer2009@icloud.com>
2023-10-15 18:26:49 +0300

    Vendor card

--- src/app/Models/ICategory.ts
+++ src/app/Models/ICategory.ts
@@ -1,5 +1,15 @@
+import { IVendor } from "./IVendor"
+
 export interface ICategory {
   id: number,
   name: string,
   photo: string
+}
+
+
+export interface ICategoryWithVendors {
+  id: number,
+  name: string,
+  photo: string,
+  vendors: IVendor[]
 }
--- src/app/Services/Category.service.ts
+++ src/app/Services/Category.service.ts
@@ -1,16 +1,19 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { ICategory } from '../Models/ICategory';
+import { ICategory, ICategoryWithVendors } from '../Models/ICategory';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CategoryService {
 
-constructor(private http:HttpClient) {}
-get():Observable<ICategory[]>{
-  return this.http.get<ICategory[]>("http://localhost:3000/categories")
-}
+  constructor(private http: HttpClient) { }
+  get(): Observable<ICategory[]> {
+    return this.http.get<ICategory[]>("http://localhost:3000/categories")
+  }
 
+  getVendorsByCategoryID(ID: number): Observable<ICategoryWithVendors> {
+    return this.http.get<ICategoryWithVendors>(`http://localhost:3000/categories/${ID}?_embed=vendors`)
+  }
 }
