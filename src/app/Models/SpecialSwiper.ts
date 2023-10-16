c8604b0a2a15127b963723207c764aabd716e02b
nazem0 <mohamednazem09@gmail.com>
2023-10-16 15:31:56 +0300

    special tinder swiper inheirts from swiper

--- src/app/Models/SpecialSwiper.ts
+++ src/app/Models/SpecialSwiper.ts
@@ -1,15 +1,7 @@
-export interface SpecialSwiper {
-    snapGrid: any;
-    activeIndex: number;
-    el: HTMLElement;
-    slides: HTMLElement[];
+import Swiper from "swiper";
+
+export interface SpecialSwiper extends Swiper {
     size: number;
-    touches: {
-      currentX: number;
-      startX: number;
-      currentY: number;
-      startY: number;
-    };
     params: {
       containerModifierClass: string;
       effect: string;
@@ -17,14 +9,8 @@ export interface SpecialSwiper {
       loop: boolean;
       // Add other parameters as needed
     };
-    originalParams: {
-      // Define the original parameters
-    };
     classNames: string[];
-    animating: boolean;
-    slideNext(): void;
     emit(event: string, data: any): void;
-    setTranslate(translate: number): void;
     tinder:any;
     // Add other methods and properties as needed
   }
