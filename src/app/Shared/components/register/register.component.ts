f1d353b6fbe60a0c7ba082a2ba0d5117ed32eb71
Samer Said <supsamer2009@icloud.com>
2023-10-18 18:43:26 +0300

    register form

--- src/app/Shared/components/register/register.component.ts
+++ src/app/Shared/components/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ScrollRevealService } from 'src/app/Services/Scroll-reveal.service';
 @Component({
   selector: 'app-register',
@@ -6,11 +7,27 @@ import { ScrollRevealService } from 'src/app/Services/Scroll-reveal.service';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
-  constructor(private ScrollReveal: ScrollRevealService) { }
+  registerForm: FormGroup;
+  data: FormData;
+  constructor(private ScrollReveal: ScrollRevealService, private builder: FormBuilder) {
+    this.data = new FormData();
+    this.registerForm = this.builder.group({
+      name: [null, [Validators.required]],
+      email: [null, [Validators.required]],
+      password: [null, [Validators.required]],
+      phoneNumber: [null, [Validators.required]],
+      SSN: [null, [Validators.required]],
+      gender: [null, [Validators.required]],
+    })
+  }
   ngOnInit() {
     const sr = this.ScrollReveal.getScrollReveal();
-    sr.reveal('.rightSide', { origin: 'right', duration: 1200,distance: '500px' })
-    sr.reveal('.leftSide', { origin: 'left', duration: 1200 ,distance: '500px'})
+    sr.reveal('.rightSide', { origin: 'right', duration: 1200, distance: '500px' })
+    sr.reveal('.leftSide', { origin: 'left', duration: 1200, distance: '500px' })
+  }
 
+  register() {
+    console.log(this.registerForm.value)
+    //////API (BOOKING REQUEST) (use FormData to collect data)
   }
 }
