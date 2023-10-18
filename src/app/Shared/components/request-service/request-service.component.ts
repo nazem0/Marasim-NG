585a56be720f5b431eba4c4a2ce92c7feda93550
Samer Said <supsamer2009@icloud.com>
2023-10-18 18:19:10 +0300

    Form Validation in Request Service

--- src/app/Shared/components/request-service/request-service.component.ts
+++ src/app/Shared/components/request-service/request-service.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { IService } from 'src/app/Models/IService';
 
 @Component({
@@ -7,5 +8,20 @@ import { IService } from 'src/app/Models/IService';
   styleUrls: ['./request-service.component.css']
 })
 export class RequestServiceComponent {
-  @Input() service :IService|null=null;
+  @Input() service: IService | null = null;
+  requestServiceForm: FormGroup;
+  data: FormData;
+
+  constructor(private builder: FormBuilder) {
+    this.data = new FormData()
+    this.requestServiceForm = this.builder.group({
+      discount: [null, [Validators.required]],
+      date: [null, [Validators.required]],
+    })
+  }
+
+  request() {
+    console.log(this.requestServiceForm.value)
+    //////API (BOOKING REQUEST) (use FormData to collect data)
+  }
 }
