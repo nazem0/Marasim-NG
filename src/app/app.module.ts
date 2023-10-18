3ef5d576f66650732719fdee5427a3d9332b9f40
nazem0_home_pc <mohamednazem09@gmail.com>
2023-10-18 18:51:36 +0300

    adjust upload file style

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { RegisterComponent } from './Shared/components/register/register.component';
 import { LoginComponent } from './Shared/components/login/login.component';
 import { PageNotFoundComponent } from './Shared/components/page-not-found/page-not-found.component';
 import { ContactComponent } from './Shared/components/contact/contact.component';
@@ -18,7 +17,6 @@ import { ReactiveFormsModule } from '@angular/forms';
 @NgModule({
   declarations: [
     AppComponent,
-    RegisterComponent,
     LoginComponent,
     PageNotFoundComponent,
     ContactComponent,
