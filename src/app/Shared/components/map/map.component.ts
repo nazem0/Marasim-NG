c381d028405b875952563da3753cedbd499f9fdd
nazem0_home_pc <mohamednazem09@gmail.com>
2023-10-19 17:07:37 +0300

    append the map js file on that specific component :p

--- src/app/Shared/components/map/map.component.ts
+++ src/app/Shared/components/map/map.component.ts
@@ -1,12 +1,17 @@
-import { Component} from '@angular/core';
+import { AfterViewInit, Component} from '@angular/core';
 
 @Component({
   selector: 'app-map',
   templateUrl: './map.component.html',
   styleUrls: ['./map.component.css']
 })
-export class MapComponent{
+export class MapComponent implements AfterViewInit{
   constructor()
   {}
+  ngAfterViewInit(): void {
+    let script=document.createElement("script")
+    script.src="src/assets/js/mapsJavaScriptAPI.js"
+    document.querySelector("head")?.append(script);
+  }
   
 }
