a5cf8177052593704caa6867a949a706d7cb2e3e
nazem0_home_pc <mohamednazem09@gmail.com>
2023-10-19 16:37:53 +0300

    temp with draggable marker map :)))))) /map

--- src/app/Shared/components/map/map.component.ts
+++ src/app/Shared/components/map/map.component.ts
@@ -1,22 +1,12 @@
-
-import { Component } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable, of } from 'rxjs';
-import { catchError, map } from 'rxjs/operators';
+import { Component} from '@angular/core';
 
 @Component({
   selector: 'app-map',
   templateUrl: './map.component.html',
   styleUrls: ['./map.component.css']
 })
-export class MapComponent {
-  apiLoaded: Observable<boolean>;
-
-  constructor(httpClient: HttpClient) {
-    this.apiLoaded = httpClient.jsonp('https://maps.googleapis.com/maps/api/js?key=AIzaSyB41DRUbKWJHPxaFjMAwdrzWzbVKartNGg', 'callback')
-        .pipe(
-          map(() => true),
-          catchError(() => of(false)),
-        );
-  }
+export class MapComponent{
+  constructor()
+  {}
+  
 }
