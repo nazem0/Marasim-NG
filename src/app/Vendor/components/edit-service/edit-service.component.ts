4463e9139da07d2543a303a9a1f38e9189b3719a
Shallaly95 <mahmoud.shallaly@gmail.com>
2023-10-21 11:46:29 +0300

    form Validation(edit service)

--- src/app/Vendor/components/edit-service/edit-service.component.ts
+++ src/app/Vendor/components/edit-service/edit-service.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-edit-service',
@@ -6,5 +8,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./edit-service.component.css']
 })
 export class EditServiceComponent {
-service: string = 'تصوير'
+  editServiceForm: FormGroup;
+  formIsValid = false;
+
+  constructor(private formBuilder: FormBuilder) {
+    this.editServiceForm = this.formBuilder.group({
+      title: ['', [Validators.required]],
+      price: [null, [Validators.required]],
+      location: [''],
+      media: [null, [Validators.required]],
+      description: [''],
+    });
+
+    this.editServiceForm.statusChanges.subscribe(() => {
+      this.formIsValid = this.editServiceForm.valid;
+    });
+  }
+  
+
+  submitEditedService() {
+    if (this.formIsValid) {
+      const formData = this.editServiceForm.value;
+      console.log(formData);
+    }
+  }
 }
