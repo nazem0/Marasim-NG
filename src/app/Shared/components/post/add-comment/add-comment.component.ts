1a3a140641f16df0835c466940dd12a514773366
nazem0_home_pc <mohamednazem09@gmail.com>
2023-10-21 19:04:07 +0300

    comment with validator

--- src/app/Shared/components/post/add-comment/add-comment.component.ts
+++ src/app/Shared/components/post/add-comment/add-comment.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
-import { FormControl } from '@angular/forms';
+import { FormControl, FormBuilder, Validators } from '@angular/forms';
 @Component({
   selector: 'app-add-comment',
   templateUrl: './add-comment.component.html',
   styleUrls: ['./add-comment.component.css']
 })
 export class AddCommentComponent {
-comment = new FormControl("comment")
-test(){
-  console.log(this.comment);
-}
+  comment = new FormControl(null,{validators:Validators.required})
+  constructor(private FormBuilder: FormBuilder) { 
+    
+  }
+  test() {
+    console.log(this.comment.value);
+  }
 }
