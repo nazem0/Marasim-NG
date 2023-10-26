29c8a1d4265f049661f3e5583eb1c4cd39af580c
nazem0_home_pc <mohamednazem09@gmail.com>
2023-10-26 10:39:24 +0300

    adjusted navbar scroll, background

--- src/app/Shared/components/landing/landing.component.ts
+++ src/app/Shared/components/landing/landing.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
 import { AuthService } from 'src/app/Services/Auth.service';
 
 @Component({
@@ -6,38 +6,33 @@ import { AuthService } from 'src/app/Services/Auth.service';
   templateUrl: './landing.component.html',
   styleUrls: ['./landing.component.css']
 })
-export class LandingComponent {
+export class LandingComponent implements OnInit, AfterViewInit {
 
 
 
 
 
-  isLogged:boolean=this.AuthService.loggedIn;
-  constructor(private AuthService:AuthService){
-
-
-    // $(function() {
-    //   $('a[href*=#]').on('click', function(e:any) {
-    //     e.preventDefault();
-    //     $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 500, 'linear');
-    //   });
-    // });
-
-  const nav = document.querySelector('nav') as HTMLElement;
-  window.addEventListener('scroll', function () {
-    if (window.scrollY > 100) {
-      nav.classList.add('bg-dark', 'shadow');
-    } else {
-      nav.classList.remove('bg-dark', 'shadow');
-    }
-  } as EventListenerOrEventListenerObject);}
-  ngOnInit(){
+  isLogged: boolean = this.AuthService.loggedIn;
+  constructor(private AuthService: AuthService) { }
+  ngOnInit() {
     console.log(this.isLogged)
-    this.isLogged=this.AuthService.loggedIn;
+    this.isLogged = this.AuthService.loggedIn;
   }
-  logout(){
-    this.AuthService.loggedIn=!this.AuthService.loggedIn;
-    this.isLogged=this.AuthService.loggedIn;
+  ngAfterViewInit(): void {
+    const nav = document.querySelector('nav') as HTMLElement;
+    window.addEventListener('scroll', function () {
+      if (window.scrollY > window.innerHeight - 100) {
+        nav.classList.add('bg-dark', 'shadow', "fixed-top");
+        nav.classList.remove("position-absolute", "top-0")
+      } else {
+        nav.classList.remove('bg-dark', 'shadow', "fixed-top");
+        nav.classList.add("position-absolute", "top-0")
+      }
+    } as EventListenerOrEventListenerObject);
+  }
+  logout() {
+    this.AuthService.loggedIn = !this.AuthService.loggedIn;
+    this.isLogged = this.AuthService.loggedIn;
   }
 
 
