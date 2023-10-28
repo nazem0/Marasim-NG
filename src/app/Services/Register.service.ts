f0c384a0b73db0e323ee7d9baca8744e77930957
nazem0_home_pc <mohamednazem09@gmail.com>
2023-10-28 03:05:51 +0200

    used environment class instead of typing the api everywhere :|

--- src/app/Services/Register.service.ts
+++ src/app/Services/Register.service.ts
@@ -2,16 +2,15 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { IRegisterUserViewModel } from '../Models/IRegisterModel';
+import { environment } from 'src/environments/environment.development';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RegisterService {
-  httpUrl: string = "https://localhost:7292/"
-
   constructor(private http: HttpClient) { }
 
   registerUser(user: any):Observable<any> {
-    return this.http.post<any>("https://localhost:7292/Account/Register", user)
+    return this.http.post<any>(`${environment.apiUrl}/Account/Register`, user)
   }
 }
