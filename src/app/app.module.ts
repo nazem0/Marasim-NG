cef6bfa4ea6ddc82e4b6380882d1d995e497b0fd
nazem0_home_pc <mohamednazem09@gmail.com>
2023-10-28 03:05:12 +0200

    attempt to use interceptor : failed :C

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HttpClientModule } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { AuthService } from './Services/Auth.service';
 import { AuthGuard } from './Guards/user.guard';
 import { ReactiveFormsModule } from '@angular/forms';
+import { InterceptorService } from './Services/interceptor.service';
 
 @NgModule({
   declarations: [
@@ -17,7 +18,13 @@ import { ReactiveFormsModule } from '@angular/forms';
     HttpClientModule,
     ReactiveFormsModule,
   ],
-  providers: [AuthService, AuthGuard],
+  providers: [AuthGuard,
+  //   {
+  //   provide:HTTP_INTERCEPTORS,
+  //   useClass:InterceptorService,
+  //   multi:true
+  // }
+],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
