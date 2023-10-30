37fcb2659f4a6549532d8791cf58a6dbee6a748b
nazem0_home_pc <mohamednazem09@gmail.com>
2023-10-30 14:37:13 +0200

    included other service endpoints to service service :|

--- src/app/Services/service.service.ts
+++ src/app/Services/service.service.ts
@@ -8,7 +8,22 @@ import { environment } from 'src/environments/environment.development';
 export class ServiceService {
 
   constructor(private HttpClient:HttpClient) { }
+  GetAll(){
+    return this.HttpClient.get(`${environment.apiUrl}/service/getall`)
+  }
+  GetById(Id:number){
+    return this.HttpClient.post(`${environment.apiUrl}/service/getbyid`,Id)
+  }
+  GetByVendorId(Id:number){
+    return this.HttpClient.post(`${environment.apiUrl}/service/getbyvendorid`,Id)
+  }
   AddService(Service:any){
     return this.HttpClient.post(`${environment.apiUrl}/service/add`,Service)
+  }
+  UpdateService(Service:any){
+    return this.HttpClient.post(`${environment.apiUrl}/service/update`,Service)
+  }
+  DeleteService(Id:number){
+    return this.HttpClient.delete(`${environment.apiUrl}/service/delete/${Id}`);
   }
 }
