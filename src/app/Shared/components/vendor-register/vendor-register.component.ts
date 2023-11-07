9d83edae57649bffb0bde6145dc7615293c374e9
nazem0_home_pc <mohamednazem09@gmail.com>
2023-11-07 12:17:36 +0200

    الشغل شغل _ مابة بنت ناس / مسروقة /

--- src/app/Shared/components/vendor-register/vendor-register.component.ts
+++ src/app/Shared/components/vendor-register/vendor-register.component.ts
@@ -79,170 +79,7 @@ export class VendorRegisterationComponent implements OnInit, AfterViewInit {
     let script = document.createElement('script');
     script.src = 'assets/js/mapsJavaScriptAPI.js';
     let mapInit = document.createElement('script');
-    mapInit.innerHTML = `
-    var map; 
-    var marker;
-    const latSpan = document.getElementById("lat");
-    const lngSpan = document.getElementById("lng");
-
-    async function initAutocomplete() {
-      
-      let pos = {
-        lat: ${this.position.lat}, 
-        lng: ${this.position.long}
-      };
-      
-      const {Map, InfoWindow} = await google.maps.importLibrary("maps");
-      const {AdvancedMarkerElement} = await google.maps.importLibrary("marker");
-    
-      map = new Map(document.getElementById("map"), {
-        zoom: 12,
-        mapTypeId: 'hybrid', 
-        center: pos,
-        mapId:"Position",
-        streetViewControl: false
-      });
-      const input = document.getElementById("pac-input");
-      const searchBox = new google.maps.places.SearchBox(input);
-      map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
-      // Bias the SearchBox results towards current map's viewport.
-      map.addListener("bounds_changed", () => {
-        searchBox.setBounds(map.getBounds());
-      });
-
-      let markers = [];
-
-
-      searchBox.addListener("places_changed", () => {
-        const places = searchBox.getPlaces();
-    
-        if (places.length == 0) {
-          return;
-        }
-    
-        // Clear out the old markers.
-        markers = [];
-        markers.forEach((marker) => {
-          marker.setMap(null);
-        });
-    
-        // For each place, get the icon, name and location.
-        const bounds = new google.maps.LatLngBounds();
-    
-        places.forEach((place) => {
-          if (!place.geometry || !place.geometry.location) {
-            console.log("Returned place contains no geometry");
-            return;
-          }
-    
-          const icon = {
-            url: place.icon,
-            size: new google.maps.Size(71, 71),
-            origin: new google.maps.Point(0, 0),
-            anchor: new google.maps.Point(17, 34),
-            scaledSize: new google.maps.Size(25, 25),
-          };
-    
-          // Create a marker for each place.
-          markers.push(
-            new google.maps.Marker({
-              map,
-              icon,
-              title: place.name,
-              position: place.geometry.location,
-            }),
-          );
-          latSpan.value=place.geometry.location.lat();
-          lngSpan.value=place.geometry.location.lng();
-          if (place.geometry.viewport) {
-            // Only geocodes have viewport.
-            bounds.union(place.geometry.viewport);
-          } else {
-            bounds.extend(place.geometry.location);
-          }
-        });
-        map.fitBounds(bounds);
-      });
-    
-    
-    window.initAutocomplete = initAutocomplete;
-
-      // Get Current Location Button
-      const locationButton = document.createElement("button");
-
-      locationButton.innerHTML = "<i class='fa-solid fa-location-dot'></i>";
-      locationButton.id="myLocation";
-      locationButton.type="button";
-      locationButton.classList.add("custom-map-control-button","btn","btn-dark","rounded-circle","p-2");
-      map.controls[google.maps.ControlPosition.TOP_CENTER].push(locationButton);
-
-      marker = new AdvancedMarkerElement({
-        map: map,
-        position: pos,
-        title: "Position",
-        gmpDraggable: true 
-      });
-      map.addListener("center_changed", () => {
-        // 3 seconds after the center of the map has changed, pan back to the
-        // marker.
-        window.setTimeout(() => {
-          map.panTo(marker.position);
-        }, 3000);
-      });
-
-      locationButton.addEventListener("click", () => {
-        // Try HTML5 geolocation.
-        if (navigator.geolocation) {
-          navigator.geolocation.getCurrentPosition(
-            (position) => {
-              const pos = {
-                lat: position.coords.latitude,
-                lng: position.coords.longitude,
-              };
-              infoWindow.setPosition(pos);
-              infoWindow.setContent("موقعك من خلال خطوط الطول ودوائر العرض: " + pos.lat + ", " + pos.lng);
-              infoWindow.open(map);
-              map.setCenter(pos);
-              marker.position=pos;
-              infoWindow.open(map, marker)
-              latSpan.value=pos.lat;
-              lngSpan.value=pos.lng;
-            },
-            () => {
-              handleLocationError(true, infoWindow, map.getCenter());
-            },
-          );
-        } else {
-          // Browser doesn't support Geolocation
-          handleLocationError(false, infoWindow, map.getCenter());
-        }
-      });
-
-
-      marker.addListener("click", () => {
-        map.setZoom(17);
-        map.setCenter(marker.position);
-      });
-    
-      let infoWindow = new InfoWindow();
-      marker.addListener("dragend", event => {
-        let newPos = marker.position;
-        infoWindow.close();
-        infoWindow.setContent("موقعك من خلال خطوط الطول ودوائر العرض: " + newPos.lat + ", " + newPos.lng);
-        latSpan.value=newPos.lat;
-        lngSpan.value=newPos.lng;
-        infoWindow.open(map, marker);
-      });
-      function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-        infoWindow.setPosition(pos);
-        infoWindow.setContent(
-          browserHasGeolocation
-            ? "Error: The Geolocation service failed."
-            : "Error: Your browser doesn't support geolocation.",
-        );
-        infoWindow.open(map);
-      }
-    }`;
+    mapInit.src='assets/js/mapInit.js';
     mapInit.type = 'text/javascript';
     document.querySelector('head')?.append(script, mapInit);
   }
@@ -285,12 +122,12 @@ export class VendorRegisterationComponent implements OnInit, AfterViewInit {
         this.RegisterService.registerVendor(this.data).subscribe({
           next: (response) => {
             console.log(response)
-            this.data.forEach(v=>console.log(v))
+            this.data.forEach(v => console.log(v))
             this.Router.navigate(["/login"]);
           },
           error: (error) => {
             console.log("object");
-            this.data.forEach(v=>console.log(v))
+            this.data.forEach(v => console.log(v))
             console.log(error);
           }
         })
