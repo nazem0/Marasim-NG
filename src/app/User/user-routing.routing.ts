8f5d560dd34f245dacbda16a253d4e8fe0b653e4
nazem0_home_pc <mohamednazem09@gmail.com>
2023-11-08 13:37:19 +0200

    vendors/vendorid

--- src/app/User/user-routing.routing.ts
+++ src/app/User/user-routing.routing.ts
@@ -44,7 +44,7 @@ const routes: Routes = [
       { path: 'generated-packages', component: GeneratedPackagesComponent },
       { path: 'contact', component: ContactComponent },
       { path: 'feed', component: FeedComponent },
-      { path: 'vendors/vendor-profile/:id', component: VendorProfileComponent },
+      { path: 'vendors/:id', component: VendorProfileComponent },
       { path: 'followings', component: FollowingsComponent },
       { path: 'checkout', component: CheckoutComponent },
       { path: 'checkout/pass', component: CheckoutPassComponent },
