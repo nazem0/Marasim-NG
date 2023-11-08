6533939804dd0fe1e7315448b67c1f08f00f8a59
nazem0_home_pc <mohamednazem09@gmail.com>
2023-11-08 16:07:02 +0200

    payment

--- src/app/User/components/checkout/checkout.component.ts
+++ src/app/User/components/checkout/checkout.component.ts
@@ -3,6 +3,7 @@ import { ReservationService } from 'src/app/Services/Reservation.service';
 import { Component } from '@angular/core';
 import { CheckoutReservation } from 'src/app/Models/Reservation';
 import { environment } from 'src/environments/environment.development';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-checkout',
@@ -11,19 +12,33 @@ import { environment } from 'src/environments/environment.development';
 })
 export class CheckoutComponent {
   apiUrl = environment.serverUrl;
+  checkoutForm: FormGroup;
+  data: FormData;
   Reservation: CheckoutReservation | null = null;
+  ReservationId = parseInt(this.ActivatedRoute.snapshot.paramMap.get("id")!)
   constructor(
     private ReservationService: ReservationService,
     private ActivatedRoute: ActivatedRoute
-  ) { }
+  ) {
+    this.data = new FormData();
+    this.checkoutForm = new FormGroup({
+      InstaPay: new FormControl('', [Validators.required]),
+      ReservationId: new FormControl(this.ReservationId)
+    });
+  }
   ngOnInit() {
-    let ReservationIdString = this.ActivatedRoute.snapshot.paramMap.get("id");
-    if (ReservationIdString != null) {
-      this.ReservationService.CheckoutReservationById(parseInt(ReservationIdString)).subscribe(
-        {
-          next:(response)=>this.Reservation=response
-        }
-      )
+    this.ReservationService.CheckoutReservationById(this.ReservationId).subscribe(
+      {
+        next: (response) => this.Reservation = response
+      }
+    )
+  }
+
+  submitForm() {
+    if(this.checkoutForm.valid){
+      this.data.set('InstaPay', this.checkoutForm.get('InstaPay')?.value);
+      this.data.set('ReservationId', this.checkoutForm.get('ReservationId')?.value);
+      console.log(this.checkoutForm.value);
     }
   }
 }
