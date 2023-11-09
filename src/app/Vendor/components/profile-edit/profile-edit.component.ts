2232083f2fd0edba9b73c1ae858207cfb413c88c
Samer Said <supsamer2009@icloud.com>
2023-11-09 11:42:50 +0200

    Vendor Update Profile

--- src/app/Vendor/components/profile-edit/profile-edit.component.ts
+++ src/app/Vendor/components/profile-edit/profile-edit.component.ts
@@ -1,23 +1,106 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { ICategory } from 'src/app/Models/ICategory';
+import { IUser } from 'src/app/Models/IUser';
+import { CategoryService } from 'src/app/Services/Category.service';
+import { CookieService } from 'ngx-cookie-service';
+import { UserService } from 'src/app/Services/User.service';
+import { IVendor } from 'src/app/Models/IVendor';
+import { VendorService } from 'src/app/Services/Vendor.service';
+import { RegisterationErrorResponse } from 'src/app/Models/RegisterationErrorResponse';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-profile-edit',
   templateUrl: './profile-edit.component.html',
   styleUrls: ['./profile-edit.component.css']
 })
-export class ProfileEditComponent {
-User={
-  name:"صهيب احمد محمد",
-  email:"sohayb@so.com",
-  phone:'01159100078',
-  password:'sohayb123'
-};
+export class ProfileEditComponent implements OnInit {
+  Categories: ICategory[] | null = null;
+  User: IUser | null = null;
+  Vendor: IVendor | null = null;
 
-cats=[
-  {name:"مصور"},
-  {name:"طباخ"},
-  {name:"منسق حفلات"},
-  {name:"قاعة"},
-]
+  @ViewChild("UploadPic") UploadPic: ElementRef | null = null;
+  PicName: string = "";
+
+  updateForm: FormGroup;
+  data: FormData;
+  PasswordRegEx = (/^(?=.*[0-9])(?=.*[!@#$%^&*])(?=.{8,})/);
+  PhoneNumberRegEx = (/^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}/);
+  ErrorResponse: RegisterationErrorResponse | null = null;
+
+
+  constructor(private CategoryService: CategoryService,
+    private UserService: UserService,
+    private CookieService: CookieService,
+    private VendorService: VendorService,
+    private builder: FormBuilder,) {
+    this.data = new FormData();
+    this.updateForm = this.builder.group({
+      Name: [this.User?.name, Validators.minLength(2)],
+      Picture: [null],
+      PhoneNumber: [this.User?.phoneNumber, [Validators.minLength(11), Validators.pattern(this.PhoneNumberRegEx)]],
+      CategoryId: [this.Vendor?.categoryId],
+      Summary: [this.Vendor?.summary, [Validators.minLength(20), Validators.maxLength(1000)]],
+    })
+  }
+
+  ngAfterViewInit(): void {
+    this.UploadPic?.nativeElement.addEventListener('change', (e: any) => {
+      this.PicName = e.target?.files[0].name;
+    });
+  }
+
+  ngOnInit() {
+    this.CategoryService.GetAll()
+      .subscribe((result) => this.Categories = result);
+
+    this.UserService.getByID(this.CookieService.get("Id"))
+      .subscribe((response) => this.User = response);
+
+    this.VendorService.GetVendorByUserId(this.CookieService.get("Id"))
+      .subscribe((result) => this.Vendor = result);
+  }
+
+
+  updateData() {
+    if (this.updateForm.valid) {
+      if(this.updateForm.get('Name')?.value)
+      {
+        this.data.set('Name', this.updateForm.get('Name')?.value);
+      }
+      if(this.updateForm.get('PhoneNumber')?.value)
+      {
+        this.data.set('PhoneNumber', this.updateForm.get('PhoneNumber')?.value);
+      }
+
+      if(this.updateForm.get('CategoryId')?.value)
+      {
+        this.data.set('CategoryId', this.updateForm.get('CategoryId')?.value);
+      }
+
+      if(this.updateForm.get('Summary')?.value)
+      {
+        this.data.set('Summary', this.updateForm.get('Summary')?.value);
+      }
+
+      if(this.UploadPic?.nativeElement.files[0])
+      {
+        this.data.set('Picture', this.UploadPic?.nativeElement.files[0]);
+      }
+
+      console.log(this.updateForm.value)
+
+      this.VendorService.UpdateVendor(this.data).subscribe({
+        next: (response) => {
+          console.log(response)
+        },
+        error: (error) => {
+          // console.log(this.data)
+          console.log(error);
+        }
+      })
+    }
+  }
+
 
 }
