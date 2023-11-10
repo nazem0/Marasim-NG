c9b2eb506de1d5eaa3aa156cfb3f1df2928d18b8
Shallaly95 <mahmoud.shallaly@gmail.com>
2023-11-10 11:43:30 +0200

    Ven reservations rebuild  design structure

--- src/app/Vendor/components/reservation/reservation-item/reservation-item.component.ts
+++ src/app/Vendor/components/reservation/reservation-item/reservation-item.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { IUser } from 'src/app/Models/IUser';
 import { VendorReservation } from 'src/app/Models/Reservation';
+import { environment } from 'src/environments/environment.development';
 
 @Component({
   selector: 'app-reservation-item',
@@ -8,14 +9,16 @@ import { VendorReservation } from 'src/app/Models/Reservation';
   styleUrls: ['./reservation-item.component.css']
 })
 export class ReservationItemComponent implements OnInit {
+  
   @Input() Reservations: VendorReservation[] = [];
   @Input() activeTab: string = 'all';
   selectedCustomer: IUser | null = null; 
+  apiUrl=environment.serverUrl;
 
 
-  ngOnInit() {
-    
-  }
+
+  ngOnInit() {}
+  
 
   openCustomerModal(User: IUser ) {
     this.selectedCustomer = User;
