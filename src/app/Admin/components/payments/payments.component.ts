77dcd21b69dfb656b3f750520c8b8efb9189856c
nazem0_home_pc <mohamednazem09@gmail.com>
2023-11-10 23:02:33 +0200

    get payments

--- src/app/Admin/components/payments/payments.component.ts
+++ src/app/Admin/components/payments/payments.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { ReservationService } from './../../../Services/Reservation.service';
+import { PaymentService } from './../../../Services/Payment.service';
+import { Payment } from 'src/app/Models/Payment';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-payments',
   templateUrl: './payments.component.html',
   styleUrls: ['./payments.component.css']
 })
-export class PaymentsComponent {
+export class PaymentsComponent implements OnInit{
+  constructor(
+    private PaymentService:PaymentService,
+    private ReservationService:ReservationService
+    ){}
+  Payments : Payment[] =[];
+  ngOnInit(): void {
+    this.PaymentService.Get().subscribe(
+      {
+        next:(response)=>this.Payments=response,
+        error:(error)=>console.log(error)
+      }
+    )
+  }
+  Confirm(){
+
+  }
 
 }
--- src/app/Services/Payment.service.ts
+++ src/app/Services/Payment.service.ts
@@ -1,6 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment.development';
+import { Payment } from '../Models/Payment';
 
 @Injectable({
   providedIn: 'root'
@@ -8,6 +10,9 @@ import { environment } from 'src/environments/environment.development';
 export class PaymentService {
 
   constructor(private HttpClient:HttpClient) { }
+  Get():Observable<Payment[]>{
+    return this.HttpClient.get<Payment[]>(`${environment.apiUrl}/Payment/Get`)
+  }
   Add(Data:any){
     return this.HttpClient.post(`${environment.apiUrl}/Payment/Add/`,Data)
   }
