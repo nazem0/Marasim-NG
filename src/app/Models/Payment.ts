37ce60ae69460a797109b3596a6d51361c78ee52
nazem0_home_pc <mohamednazem09@gmail.com>
2023-11-10 23:05:12 +0200

    model naming fixed

--- src/app/Models/Payment.ts
+++ src/app/Models/Payment.ts
@@ -1,9 +1,9 @@
 import { UserMinInfo } from "./IUser";
-import { VendorMinInfo } from "./IVendor";
+import { IVendorMinInfo } from "./IVendor";
 
 export interface Payment {
     instaPay: string,
     servicePrice:number,
     user: UserMinInfo,
-    vendor: VendorMinInfo,
+    vendor: IVendorMinInfo,
 }
