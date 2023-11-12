946be930839c6edff2cc4ab8cdd3199c3404a336
nazem0 <mohamednazem09@gmail.com>
2023-11-12 15:03:27 +0200

    logout without navigate

--- src/app/Services/Auth.service.ts
+++ src/app/Services/Auth.service.ts
@@ -46,7 +46,6 @@ export class AuthService {
     console.log("cookies removed");
     this.CookieService.deleteAll();
     location.reload();
-    this.Router.navigateByUrl('/');
     this._isLoggedIn.next(false);
   }
   logout(){
