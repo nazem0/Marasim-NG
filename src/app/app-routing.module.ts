ac7616fff7e56126cd97408063a82ff72b92a41c
nazem0 <mohamednazem09@gmail.com>
2023-11-13 21:01:35 +0200

    vendor guard not user guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PageNotFoundComponent } from './Shared/components/page-not-found/page-not-found.component';
-import { AuthGuard } from './Guards/user.guard';
+import { AuthGuard } from './Guards/vendor.guard';
 
 const routes: Routes = [
 
