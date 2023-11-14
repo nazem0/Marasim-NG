3599a34088dc00ea59fc88528123ea786c966d5e
sohayb ahmed <engsohayb97@gmail.com>
2023-11-14 13:02:22 +0200

    add scroll revel to landing + some edits in user home page

--- src/app/Shared/components/landing/landing.component.ts
+++ src/app/Shared/components/landing/landing.component.ts
@@ -2,6 +2,7 @@ import { AfterViewInit, Component, HostListener, OnInit } from '@angular/core';
 import { IServiceAttachmentCustom } from 'src/app/Models/IService';
 import { AuthService } from 'src/app/Services/Auth.service';
 import { AttachmentService } from 'src/app/Services/Attachment.service';
+import { ScrollRevealService } from 'src/app/Services/Scroll-reveal.service';
 
 @Component({
   selector: 'app-landing',
@@ -12,11 +13,25 @@ export class LandingComponent implements OnInit, AfterViewInit {
   slides: IServiceAttachmentCustom[] = [];
 
   isLogged: boolean = this.AuthService.loggedIn;
-  constructor(private AuthService: AuthService, private AttachmentService: AttachmentService) { }
+  constructor(private AuthService: AuthService, private AttachmentService: AttachmentService, private ScrollReveal : ScrollRevealService) { }
   ngOnInit() {
     console.log(this.isLogged)
     this.isLogged = this.AuthService.loggedIn;
     this.AttachmentService.GetAllCustom().subscribe((result) => this.slides = result)
+
+    const sr = this.ScrollReveal.getScrollReveal();
+    sr.reveal('.map-img', { origin: 'left', duration: 2000, distance: '500px' })
+    sr.reveal('.txt1', { origin: 'right', duration: 2000, distance: '500px' })
+    sr.reveal('.txt2', { origin: 'right', duration: 2100, distance: '500px' })
+    sr.reveal('.txt3', { origin: 'right', duration: 2200, distance: '500px' })
+    sr.reveal('.txt4', { origin: 'right', duration: 2300, distance: '500px' })
+    sr.reveal('.ic1', { origin: 'right', duration: 2300, distance: '500px' })
+    sr.reveal('.ic2' , { origin: 'right', duration: 2400, distance: '500px' })
+    sr.reveal('.ic3', { origin: 'right', duration: 2500, distance: '500px' })
+    sr.reveal('.ic4', { origin: 'right', duration: 2600, distance: '500px' })
+    sr.reveal('.cd1' , { origin: 'left', duration: 2000, distance: '500px' })
+    sr.reveal('.cd2', { origin: 'left', duration: 2100, distance: '500px' })
+    sr.reveal('.cd3', { origin: 'left', duration: 2200, distance: '500px' })
   }
   ngAfterViewInit(): void {
     const nav = document.querySelector('nav') as HTMLElement;
