7f2f84d11cf0456a2df89cb6dbff9cff6bba96ba
nazem0_home_pc <mohamednazem09@gmail.com>
2023-11-14 07:30:16 +0200

    To be Continued

--- src/app/User/components/generate-packages/generate-packages.component.ts
+++ src/app/User/components/generate-packages/generate-packages.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { CityService } from './../../../Services/city.service';
+import { GovernorateService } from './../../../Services/governorate.service';
+import { Component, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { City } from 'src/app/Models/City';
+import { Governorate } from 'src/app/Models/governorate';
 
 @Component({
   selector: 'app-generate-packages',
   templateUrl: './generate-packages.component.html',
   styleUrls: ['./generate-packages.component.css']
 })
-export class GeneratePackagesComponent {
-
+export class GeneratePackagesComponent implements AfterViewInit {
+  @ViewChild("gov") gov: ElementRef | null = null;
+  form: FormGroup;
+  data: FormData;
+  cities: City[] = [];
+  govs: Governorate[] = [];
+  constructor(
+    private fb: FormBuilder,
+    private GovernorateService: GovernorateService,
+    private CityService: CityService
+  ) {
+    this.GovernorateService.get().subscribe((resp) => this.govs = resp);
+    this.data = new FormData();
+    this.form = this.fb.group({
+      Budget: [null, [Validators.required]],
+      Categories: [null, [Validators.required]],
+      GovernorateId: [null],
+      CityId: [null],
+      Rate: [null]
+    })
+  }
+  ngAfterViewInit(): void {
+    this.gov?.nativeElement.addEventListener('change', (e: any) => {
+      this.CityService.getByGovId(this.gov?.nativeElement.value).subscribe((resp) => this.cities = resp)
+    });
+  }
 }
