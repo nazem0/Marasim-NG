d6862495142f0735370dc8222898dd9701ef17ce
Samer Said <supsamer2009@icloud.com>
2023-11-14 19:39:17 +0200

    NO JS ALLOWED

--- src/app/Shared/components/landing/landing.component.ts
+++ src/app/Shared/components/landing/landing.component.ts
@@ -22,31 +22,31 @@ export class LandingComponent implements OnInit, AfterViewInit {
     this.isLogged = this.AuthService.loggedIn;
     this.VendorService.GetVendorsMidInfo().subscribe((result) => this.slides = result)
 
-    const sr = this.ScrollReveal.getScrollReveal();
-    sr.reveal('.map-img', { origin: 'left', duration: 2000, distance: '500px' })
-    sr.reveal('.txt1', { origin: 'right', duration: 2000, distance: '500px' })
-    sr.reveal('.txt2', { origin: 'right', duration: 2100, distance: '500px' })
-    sr.reveal('.txt3', { origin: 'right', duration: 2200, distance: '500px' })
-    sr.reveal('.txt4', { origin: 'right', duration: 2300, distance: '500px' })
-    sr.reveal('.ic1', { origin: 'right', duration: 2300, distance: '500px' })
-    sr.reveal('.ic2' , { origin: 'right', duration: 2400, distance: '500px' })
-    sr.reveal('.ic3', { origin: 'right', duration: 2500, distance: '500px' })
-    sr.reveal('.ic4', { origin: 'right', duration: 2600, distance: '500px' })
-    sr.reveal('.cd1' , { origin: 'left', duration: 2000, distance: '500px' })
-    sr.reveal('.cd2', { origin: 'left', duration: 2100, distance: '500px' })
-    sr.reveal('.cd3', { origin: 'left', duration: 2200, distance: '500px' })
+    // const sr = this.ScrollReveal.getScrollReveal();
+    // sr.reveal('.map-img', { origin: 'left', duration: 2000, distance: '500px' })
+    // sr.reveal('.txt1', { origin: 'right', duration: 2000, distance: '500px' })
+    // sr.reveal('.txt2', { origin: 'right', duration: 2100, distance: '500px' })
+    // sr.reveal('.txt3', { origin: 'right', duration: 2200, distance: '500px' })
+    // sr.reveal('.txt4', { origin: 'right', duration: 2300, distance: '500px' })
+    // sr.reveal('.ic1', { origin: 'right', duration: 2300, distance: '500px' })
+    // sr.reveal('.ic2' , { origin: 'right', duration: 2400, distance: '500px' })
+    // sr.reveal('.ic3', { origin: 'right', duration: 2500, distance: '500px' })
+    // sr.reveal('.ic4', { origin: 'right', duration: 2600, distance: '500px' })
+    // sr.reveal('.cd1' , { origin: 'left', duration: 2000, distance: '500px' })
+    // sr.reveal('.cd2', { origin: 'left', duration: 2100, distance: '500px' })
+    // sr.reveal('.cd3', { origin: 'left', duration: 2200, distance: '500px' })
   }
   ngAfterViewInit(): void {
-    const nav = document.querySelector('nav') as HTMLElement;
-    window.addEventListener('scroll', function () {
-      if (window.scrollY > window.innerHeight - 100) {
-        nav.classList.add('bg-dark', 'shadow', "fixed-top");
-        nav.classList.remove("position-absolute", "top-0")
-      } else {
-        nav.classList.remove('bg-dark', 'shadow', "fixed-top");
-        nav.classList.add("position-absolute", "top-0")
-      }
-    } as EventListenerOrEventListenerObject);
+    // const nav = document.querySelector('nav') as HTMLElement;
+    // window.addEventListener('scroll', function () {
+    //   if (window.scrollY > window.innerHeight - 100) {
+    //     nav.classList.add('bg-dark', 'shadow', "fixed-top");
+    //     nav.classList.remove("position-absolute", "top-0")
+    //   } else {
+    //     nav.classList.remove('bg-dark', 'shadow', "fixed-top");
+    //     nav.classList.add("position-absolute", "top-0")
+    //   }
+    // } as EventListenerOrEventListenerObject);
   }
 
 
@@ -72,16 +72,16 @@ export class LandingComponent implements OnInit, AfterViewInit {
   // HELP ME // HELP ME // HELP ME // HELP ME // HELP ME 
   // HELP ME // HELP ME // HELP ME // HELP ME // HELP ME 
   // HELP ME // HELP ME // HELP ME // HELP ME // HELP ME 
-  @HostListener('window:scroll', ['$event'])
-  onScroll() {
-    const section = document.querySelector('.section-box') as HTMLElement;
-    // const sectionTop = section.getBoundingClientRect().top;
+  // @HostListener('window:scroll', ['$event'])
+  // onScroll() {
+  //   const section = document.querySelector('.section-box') as HTMLElement;
+  //   const sectionTop = section.getBoundingClientRect().top;
 
-    // if (sectionTop <= 0) {
-    //   this.divPosition = 'static'; // Switch to normal position
-    // } else {
-    //   this.divPosition = 'fixed'; // Stick to the bottom
-    // }
-  }
+  //   if (sectionTop <= 0) {
+  //     this.divPosition = 'static'; // Switch to normal position
+  //   } else {
+  //     this.divPosition = 'fixed'; // Stick to the bottom
+  //   }
+  // }
 }
 
