6a88fc32b7dca651e69c2b8df4e62426c422cdbe
nazem0_home_pc <mohamednazem09@gmail.com>
2023-11-16 21:03:00 +0200

    edit user basic info -- working

--- src/app/Services/User.service.ts
+++ src/app/Services/User.service.ts
@@ -10,22 +10,23 @@ import { environment } from 'src/environments/environment.development';
   providedIn: 'root'
 })
 export class UserService {
-  constructor(private http:HttpClient) { }
+  constructor(private HttpClient: HttpClient) { }
 
   // get(){
   //   return this.http.get<IUser[]>("http://localhost:3000/users/")
   // }
 
-  getById(Id:string|null=null):Observable<IUser>
-  {
-    if(Id){
-      return this.http.get<IUser>(`${environment.apiUrl}/user/UserDetails/${Id}`)
+  getById(Id: string | null = null): Observable<IUser> {
+    if (Id) {
+      return this.HttpClient.get<IUser>(`${environment.apiUrl}/user/UserDetails/${Id}`)
     }
-    else{
+    else {
       throw "Check Id";
     }
   }
-
+  UpdateVendor(updateData: FormData) {
+    return this.HttpClient.put(`${environment.apiUrl}/User/Update`, updateData)
+  }
   // getFollowing(Id:number){
   //   return this.http.get<any>(`http://localhost:3000/follows?userId=${Id}`)
   // }
