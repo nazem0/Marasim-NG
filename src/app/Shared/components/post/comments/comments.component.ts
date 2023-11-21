b53c565d17833f69b1d0439d5a97340b03adef7f
nazem0_home_pc <mohamednazem09@gmail.com>
2023-11-21 16:18:58 +0200

    paginated comments + show last added comment immediately

--- src/app/Shared/components/post/comments/comments.component.ts
+++ src/app/Shared/components/post/comments/comments.component.ts
@@ -1,5 +1,6 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { IComment } from 'src/app/Models/IPost';
+import { PaginationViewModel } from 'src/app/Models/PaginationViewModel';
 import { AuthService } from 'src/app/Services/Auth.service';
 import { PostService } from 'src/app/Services/Post.service';
 
@@ -8,14 +9,78 @@ import { PostService } from 'src/app/Services/Post.service';
   templateUrl: './comments.component.html',
   styleUrls: ['./comments.component.css']
 })
-export class CommentsComponent {
-  @Input() postId!:number;
-  comments: IComment[]=[]
-  
-  constructor(private post:PostService, public AuthService: AuthService) {
+export class CommentsComponent implements OnInit {
+  newComment = false;
+  pageIndex = 1;
+  pageSize = 5;
+  @Input() postId!: number;
+  commentsCount: number | null = null;
+  comments: PaginationViewModel<IComment> = {
+    data: [],
+    count: 0,
+    length: 0,
+    pageIndex: 0,
+    pageSize: 0,
+    lastPage: 0
+  };
+
+  constructor(private postService: PostService, public AuthService: AuthService) {
+  }
+  ngOnInit(): void {
+    this.getCommentsCount();
   }
 
-  ngOnInit(){
-    this.post.GetCommentsByPostId(this.postId).subscribe((result)=>this.comments=result)
+
+  getData() {
+    console.log("clicked");
+    this.postService.GetCommentsByPostId(this.postId, this.pageIndex, this.pageSize).subscribe(
+      {
+        next: (result) => {
+          this.comments.count = result.count
+          this.comments.lastPage = result.lastPage;
+          this.comments.pageIndex = result.pageIndex;
+          this.comments.pageSize = result.pageSize;
+          result.data.forEach(comment => {
+            if (!!this.comments.data.find(c => c.id == comment.id)) return;
+            this.comments.data.push(comment)
+          })
+          if (result.data.length == this.pageSize) {
+            this.pageIndex++;
+          }
+        },
+        error: error => console.log(error)
+      })
+  }
+
+  getLastComment() {
+    this.newComment = !this.newComment
+    if (this.newComment && this.comments.data.length == this.comments.count)
+      this.postService.GetCommentsByPostId(this.postId, this.pageIndex, this.pageSize).subscribe(
+        {
+          next: (result) => {
+            this.getCommentsCount();
+            this.comments.count = result.count
+            this.comments.lastPage = result.lastPage;
+            this.comments.pageIndex = result.pageIndex;
+            this.comments.pageSize = result.pageSize;
+            result.data.forEach(comment => {
+              if (!!this.comments.data.find(c => c.id == comment.id)) return;
+              this.comments.data.push(comment)
+            })
+            if (result.data.length == this.pageSize) {
+              this.pageIndex++;
+            }
+            this.newComment = !this.newComment;
+          },
+          error: error => console.log(error)
+        })
+  }
+
+  getCommentsCount() {
+    this.postService.GetCommentsCountByPostId(this.postId).subscribe({
+      next: commentsCount => {
+        this.commentsCount = commentsCount
+      }
+    })
   }
 }
