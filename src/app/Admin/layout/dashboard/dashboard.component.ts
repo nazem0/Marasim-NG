3a736012901ea91543b962f18e85045deb12280b
Samer Said <supsamer2009@icloud.com>
2023-11-23 19:09:52 +0200

    admin side dashboard logout

--- src/app/Admin/layout/dashboard/dashboard.component.ts
+++ src/app/Admin/layout/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AuthService } from 'src/app/Services/Auth.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -6,20 +7,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent {
+  sidebarOpen = false;
 
-   sidebarOpen = false;
+  constructor(public AuthService: AuthService) { }
 
   menuBtnChange(): void {
     if (this.sidebarOpen) {
-      document.getElementById('btn')?.classList.replace('fa-solid fa-bars', 'fa-solid fa-xmark');
+      document.getElementById('btn')?.classList.replace('fa-bars', 'fa-xmark');
     } else {
-      document.getElementById('btn')?.classList.replace('fa-solid fa-xmark', 'fa-solid fa-bars');
+      document.getElementById('btn')?.classList.replace('fa-xmark', 'fa-bars');
     }
   }
 
   toggleSidebar(): void {
     this.sidebarOpen = !this.sidebarOpen;
     this.menuBtnChange();
+  }
+
+
+  logout() {
+    this.AuthService.logout();
+    this.AuthService.removeCookies();
   }
 
 }
