2e032626a716bbc3383cbb772510b115f2ae2ec1
nazem0_home_pc <mohamednazem09@gmail.com>
2023-11-24 00:07:25 +0200

    give category id, gives u picurl

--- src/app/Services/Category.service.ts
+++ src/app/Services/Category.service.ts
@@ -9,25 +9,66 @@ import { CategoryName } from '../Models/CategoryName';
   providedIn: 'root'
 })
 export class CategoryService {
-
+  categoriesPictures = [
+    {
+      categoryId: 1,
+      picUrl: "/assets/img/photo_session.webp"
+    },
+    {
+      categoryId: 2,
+      picUrl: "/assets/img/photo_session.webp"
+    },
+    {
+      categoryId: 3,
+      picUrl: "/assets/img/photo_session.webp"
+    },
+    {
+      categoryId: 4,
+      picUrl: "/assets/img/photo_session.webp"
+    },
+    {
+      categoryId: 5,
+      picUrl: "/assets/img/chefs.webp"
+    },
+    {
+      categoryId: 6,
+      picUrl: "/assets/img/photo_session.webp"
+    },
+    {
+      categoryId: 7,
+      picUrl: "/assets/img/photo_session.webp"
+    },
+    {
+      categoryId: 8,
+      picUrl: "/assets/img/photo_session.webp"
+    }
+  ]
   constructor(private HttpClient: HttpClient) { }
   GetAll(): Observable<ICategory[]> {
     return this.HttpClient.get<ICategory[]>(`${environment.apiUrl}/Category/GetAll`)
   }
 
-  GetById(categoryId : number ): Observable<ICategory> {
+  GetById(categoryId: number): Observable<ICategory> {
     return this.HttpClient.get<ICategory>(`${environment.apiUrl}/Category/GetById/${categoryId}`)
   }
 
-  GetByVendorId(vendorId : number ): Observable<ICategory> {
+  GetByVendorId(vendorId: number): Observable<ICategory> {
     return this.HttpClient.get<ICategory>(`${environment.apiUrl}/Category/GetByVendorId/${vendorId}`)
   }
 
-  GetNames(): Observable<CategoryName[]>{
+  GetNames(): Observable<CategoryName[]> {
     return this.HttpClient.get<CategoryName[]>(`${environment.apiUrl}/Category/GetNames`)
   }
 
-  AddCategory(Category: any){
+  AddCategory(Category: any) {
     return this.HttpClient.post(`${environment.apiUrl}/Category/Add`, Category)
+  }
+  getCategoryPicture(categoryId: number) {
+    let picUrl = this.categoriesPictures.find(cat => cat.categoryId == categoryId)?.picUrl
+    if (picUrl)
+      return picUrl
+    else
+      return "/assets/img/wedding-boom.jpg"
+
   }
 }
