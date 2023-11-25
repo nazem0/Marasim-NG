c7502c55040a3c973c99c1ab0349a983dd35e7c6
nazem0_home_pc <mohamednazem09@gmail.com>
2023-11-25 03:33:21 +0200

    end any modal-backdrop at navigation end

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -7,5 +9,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Marasim';
+  constructor(private router:Router){
+    this.router.events.pipe(filter(event => event instanceof NavigationEnd))
+    .subscribe(() => {
+      document.querySelector(".modal-backdrop")?.remove();
+    });
+  }
 }
 
