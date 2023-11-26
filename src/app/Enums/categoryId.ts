edaf596aa733f53038050f9d77864bc97e35112c
nazem0_home_pc <mohamednazem09@gmail.com>
2023-11-26 05:33:42 +0200

    CategoryId Enum

---
+++ src/app/Enums/categoryId.ts
@@ -0,0 +1,11 @@
+export enum CategoryId {
+    _1hall = 1,
+    _2islamicWeddingHall = 2,
+    _3club = 3,
+    _4photographer = 4,
+    _5restaurant = 5,
+    _6beautySalon = 6,
+    _7hairdresserMale = 7,
+    _8hairdresserFemale = 8,
+    _9weddingProcession = 9,
+  }
