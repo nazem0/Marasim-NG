1ab0f3ea1ffc4be6d47b2b13d41491050243b9da
nazem0_home_pc <mohamednazem09@gmail.com>
2023-11-26 20:30:59 +0200

    one landing page

--- src/app/Shared/components/landing/landing.component.ts
+++ src/app/Shared/components/landing/landing.component.ts
@@ -1,25 +1,23 @@
-import { AfterViewInit, Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit, OnDestroy } from '@angular/core';
 import { AuthService } from 'src/app/Services/Auth.service';
 import { ScrollRevealService } from 'src/app/Services/Scroll-reveal.service';
 import { VendorService } from 'src/app/Services/Vendor.service';
 import { IVendorMidInfo } from 'src/app/Models/IVendor';
+import { eventListeners } from '@popperjs/core';
 
 @Component({
   selector: 'app-landing',
   templateUrl: './landing.component.html',
   styleUrls: ['./landing.component.css']
 })
-export class LandingComponent implements OnInit, AfterViewInit {
+export class LandingComponent implements OnInit, AfterViewInit, OnDestroy {
   slides: IVendorMidInfo[] = [];
-
-  isLogged: boolean = this.AuthService.loggedIn;
   constructor(
-    private AuthService: AuthService,
+    public AuthService: AuthService,
     private VendorService: VendorService,
     private ScrollReveal: ScrollRevealService) { }
+
   ngOnInit() {
-    console.log(this.isLogged)
-    this.isLogged = this.AuthService.loggedIn;
     this.VendorService.GetIntOfVendors()
       .subscribe({
         next: (result) => {
@@ -30,38 +28,45 @@ export class LandingComponent implements OnInit, AfterViewInit {
         }
       })
 
-    // const sr = this.ScrollReveal.getScrollReveal();
-    // sr.reveal('.map-img', { origin: 'left', duration: 2000, distance: '500px' })
-    // sr.reveal('.txt1', { origin: 'right', duration: 2000, distance: '500px' })
-    // sr.reveal('.txt2', { origin: 'right', duration: 2100, distance: '500px' })
-    // sr.reveal('.txt3', { origin: 'right', duration: 2200, distance: '500px' })
-    // sr.reveal('.txt4', { origin: 'right', duration: 2300, distance: '500px' })
-    // sr.reveal('.ic1', { origin: 'right', duration: 2300, distance: '500px' })
-    // sr.reveal('.ic2' , { origin: 'right', duration: 2400, distance: '500px' })
-    // sr.reveal('.ic3', { origin: 'right', duration: 2500, distance: '500px' })
-    // sr.reveal('.ic4', { origin: 'right', duration: 2600, distance: '500px' })
-    // sr.reveal('.cd1' , { origin: 'left', duration: 2000, distance: '500px' })
-    // sr.reveal('.cd2', { origin: 'left', duration: 2100, distance: '500px' })
-    // sr.reveal('.cd3', { origin: 'left', duration: 2200, distance: '500px' })
+    const sr = this.ScrollReveal.getScrollReveal();
+    sr.reveal('.map-img', { origin: 'left', duration: 2000, distance: '500px' })
+    sr.reveal('.txt1', { origin: 'right', duration: 2000, distance: '500px' })
+    sr.reveal('.txt2', { origin: 'right', duration: 2100, distance: '500px' })
+    sr.reveal('.txt3', { origin: 'right', duration: 2200, distance: '500px' })
+    sr.reveal('.txt4', { origin: 'right', duration: 2300, distance: '500px' })
+    sr.reveal('.ic1', { origin: 'right', duration: 2300, distance: '500px' })
+    sr.reveal('.ic2' , { origin: 'right', duration: 2400, distance: '500px' })
+    sr.reveal('.ic3', { origin: 'right', duration: 2500, distance: '500px' })
+    sr.reveal('.ic4', { origin: 'right', duration: 2600, distance: '500px' })
+    sr.reveal('.cd1' , { origin: 'left', duration: 2000, distance: '500px' })
+    sr.reveal('.cd2', { origin: 'left', duration: 2100, distance: '500px' })
+    sr.reveal('.cd3', { origin: 'left', duration: 2200, distance: '500px' })
   }
   ngAfterViewInit(): void {
-    const nav = document.querySelector('nav') as HTMLElement;
-    window.addEventListener('scroll', function () {
-      if (window.scrollY > window.innerHeight - 100) {
-        nav.classList.add('bg-dark', 'shadow', "fixed-top");
-        nav.classList.remove("position-absolute", "top-0")
-      } else {
-        nav.classList.remove('bg-dark', 'shadow', "fixed-top");
-        nav.classList.add("position-absolute", "top-0")
-      }
-    } as EventListenerOrEventListenerObject);
+    let nav = document.querySelector('.navNotLoggedIn');
+    if (nav) {
+      nav.classList.remove("bg-dark")
+      window.addEventListener("scroll", this.scrollHandler);
+    }
   }
 
-
-
+  ngOnDestroy(): void {
+    window.removeEventListener("scroll", this.scrollHandler)
+  }
+  scrollHandler(): void {
+    let nav = document.querySelector('nav');
+    if (nav) {
+      if (window.scrollY > window.innerHeight - 100) {
+        nav.classList.add('bg-dark', 'shadow', 'fixed-top');
+        nav.classList.remove('position-absolute', 'top-0');
+      } else {
+        nav.classList.remove('bg-dark', 'shadow', 'fixed-top');
+        nav.classList.add('position-absolute', 'top-0');
+      }
+    }
+  }
   logout() {
-    this.AuthService.loggedIn = !this.AuthService.loggedIn;
-    this.isLogged = this.AuthService.loggedIn;
+    this.AuthService.logout();
   }
 
   go() {
@@ -91,5 +96,4 @@ export class LandingComponent implements OnInit, AfterViewInit {
   //     this.divPosition = 'fixed'; // Stick to the bottom
   //   }
   // }
-}
-
+} 
---
+++ src/app/Shared/components/landing/walkthrough/walkthrough.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-walkthrough',
+  templateUrl: './walkthrough.component.html',
+  styleUrls: ['./walkthrough.component.css']
+})
+export class WalkthroughComponent {
+
+}
