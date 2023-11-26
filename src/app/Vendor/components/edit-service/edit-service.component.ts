617e1cf4de75897c73b299ad70ddc5f47cfd5e56
nazem0_home_pc <mohamednazem09@gmail.com>
2023-11-27 00:35:23 +0200

    separate pages instead of modals

--- src/app/Vendor/components/edit-service/edit-service.component.ts
+++ src/app/Vendor/components/edit-service/edit-service.component.ts
@@ -1,5 +1,7 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 import { IService } from 'src/app/Models/IService';
 import { ServiceService } from 'src/app/Services/service.service';
 import { environment } from 'src/environments/environment';
@@ -12,21 +14,35 @@ import { environment } from 'src/environments/environment';
 })
 export class EditServiceComponent {
   apiUrl = environment.serverUrl;
-  @Input() service: IService | null = null;
-  @Output() refresh = new EventEmitter();
+  service: IService | null = null;
   editServiceForm: FormGroup;
   data: FormData;
   formIsValid = false;
 
-  constructor(private formBuilder: FormBuilder, private ServiceService: ServiceService) {
+  constructor(
+    private formBuilder: FormBuilder,
+    private ServiceService: ServiceService,
+    private activatedRoute: ActivatedRoute,
+    private router: Router,
+    private toastr:ToastrService
+  ) {
+    this.activatedRoute.paramMap.subscribe({
+      next: params => {
+        let serviceIdParam = parseInt(params.get("serviceId")!);
+        if (isNaN(serviceIdParam))
+          this.router.navigate(["/404"]);
+        else
+          this.ServiceService.GetById(serviceIdParam).subscribe({
+            next: service => this.service = service
+          })
+      }
+    })
     this.data = new FormData();
     this.editServiceForm = this.formBuilder.group({
       Title: [this.service?.title, [Validators.minLength(3), Validators.maxLength(50)]],
       Price: [this.service?.price],
-      // media: [null],
       Description: [this.service?.description, [Validators.minLength(20), Validators.maxLength(1000)]],
     });
-
     this.editServiceForm.statusChanges.subscribe(() => {
       this.formIsValid = this.editServiceForm.valid;
     });
@@ -50,11 +66,9 @@ export class EditServiceComponent {
 
       this.ServiceService.UpdateService(this.data, this.service?.id!)
         .subscribe({
-          next: (data) => {
-            console.log("Service Updated");
-            this.refresh.emit();
-            this.editServiceForm.reset();
-            this.data = new FormData();
+          next: () => {
+            this.toastr.success("تم التعديل بنجاح")
+            this.router.navigate([".."],{relativeTo:this.activatedRoute});
           },
           error: (error) => {
             console.log(error);
