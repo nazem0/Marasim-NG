29f750c74c9d2327299a70e73f89e4c0823006e8
nazem0_home_pc <mohamednazem09@gmail.com>
2023-11-26 05:34:06 +0200

    set certain img to each category

--- src/app/Services/Category.service.ts
+++ src/app/Services/Category.service.ts
@@ -3,78 +3,81 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { CategoryWithMinMaxViewModel, CategoryName, ICategory } from '../Models/ICategory';
 import { environment } from 'src/environments/environment.development';
+import { CategoryId } from '../Enums/categoryId';
 
 @Injectable({
   providedIn: 'root'
 })
+
 export class CategoryService {
-  categoriesPictures = [
-    {
-      categoryId: 1,
-      picUrl: "/assets/img/photo_session.webp"
-    },
-    {
-      categoryId: 2,
-      picUrl: "/assets/img/photo_session.webp"
-    },
-    {
-      categoryId: 3,
-      picUrl: "/assets/img/photo_session.webp"
-    },
-    {
-      categoryId: 4,
-      picUrl: "/assets/img/photo_session.webp"
-    },
-    {
-      categoryId: 5,
-      picUrl: "/assets/img/chefs.webp"
-    },
-    {
-      categoryId: 6,
-      picUrl: "/assets/img/photo_session.webp"
-    },
-    {
-      categoryId: 7,
-      picUrl: "/assets/img/photo_session.webp"
-    },
-    {
-      categoryId: 8,
-      picUrl: "/assets/img/photo_session.webp"
-    }
-  ]
-  constructor(private HttpClient: HttpClient) { }
-  GetAll(): Observable<ICategory[]> {
-    return this.HttpClient.get<ICategory[]>(`${environment.apiUrl}/Category/GetAll`)
+  
+categoriesPictures = [
+  {
+    categoryId: CategoryId._1hall,
+    picUrl: "/assets/img/Categories/Hall.webp"
+  },
+  {
+    categoryId: CategoryId._2islamicWeddingHall,
+    picUrl: "/assets/img/Categories/IslamicWeddingHall.jpg"
+  },
+  {
+    categoryId: CategoryId._3club,
+    picUrl: "/assets/img/Categories/Club.webp"
+  },
+  {
+    categoryId: CategoryId._4photographer,
+    picUrl: "/assets/img/Categories/Photographer.jpg"
+  },
+  {
+    categoryId: CategoryId._5restaurant,
+    picUrl: "/assets/img/Categories/Restaurant.webp"
+  },
+  {
+    categoryId: CategoryId._6beautySalon,
+    picUrl: "/assets/img/Categories/BeautySalon.jpg"
+  },
+  {
+    categoryId: CategoryId._7hairdresserMale,
+    picUrl: "/assets/img/Categories/Hall.webp"
+  },
+  {
+    categoryId: CategoryId._8hairdresserFemale,
+    picUrl: "/assets/img/Categories/HairdresserFemale.webp"
   }
+]
+constructor(private HttpClient: HttpClient) { }
+GetAll(): Observable < ICategory[] > {
+  return this.HttpClient.get<ICategory[]>(`${environment.apiUrl}/Category/GetAll`)
+}
 
-  GetById(categoryId: number): Observable<ICategory> {
-    return this.HttpClient.get<ICategory>(`${environment.apiUrl}/Category/GetById/${categoryId}`)
-  }
+GetById(categoryId: number): Observable < ICategory > {
+  return this.HttpClient.get<ICategory>(`${environment.apiUrl}/Category/GetById/${categoryId}`)
+}
 
-  GetByVendorId(vendorId: number): Observable<ICategory> {
-    return this.HttpClient.get<ICategory>(`${environment.apiUrl}/Category/GetByVendorId/${vendorId}`)
-  }
+GetByVendorId(vendorId: number): Observable < ICategory > {
+  return this.HttpClient.get<ICategory>(`${environment.apiUrl}/Category/GetByVendorId/${vendorId}`)
+}
 
-  GetNames(): Observable<CategoryName[]> {
-    return this.HttpClient.get<CategoryName[]>(`${environment.apiUrl}/Category/GetNames`)
-  }
+GetNames(): Observable < CategoryName[] > {
+  return this.HttpClient.get<CategoryName[]>(`${environment.apiUrl}/Category/GetNames`)
+}
 
 
-  Count(): Observable<number> {
-    return this.HttpClient.get<number>(`${environment.apiUrl}/Category/Count`)
-  }
+Count(): Observable < number > {
+  return this.HttpClient.get<number>(`${environment.apiUrl}/Category/Count`)
+}
 
-  AddCategory(Category: any) {
-    return this.HttpClient.post(`${environment.apiUrl}/Category/Add`, Category)
-  }
-  getCategoryPicture(categoryId: number) {
-    let picUrl = this.categoriesPictures.find(cat => cat.categoryId == categoryId)?.picUrl
-    if (picUrl)
-      return picUrl
-    else
-      return "/assets/img/wedding-boom.jpg"
-  }
-  GetCategoriesWithMinMax():Observable<CategoryWithMinMaxViewModel[]>{
-    return this.HttpClient.get<CategoryWithMinMaxViewModel[]>(`${environment.apiUrl}/Category/GetCategoriesWithMinMax`)
-  }
+AddCategory(Category: any) {
+  return this.HttpClient.post(`${environment.apiUrl}/Category/Add`, Category)
+}
+getCategoryPicture(categoryId: number) {
+  let picUrl = this.categoriesPictures.find(cat => cat.categoryId == categoryId)?.picUrl
+  if (picUrl)
+    return picUrl
+  else
+    return "/assets/img/wedding-boom.jpg"
+}
+GetCategoriesWithMinMax(): Observable < CategoryWithMinMaxViewModel[] > {
+  return this.HttpClient.get<CategoryWithMinMaxViewModel[]>(`${environment.apiUrl}/Category/GetCategoriesWithMinMax`)
+}
 }
